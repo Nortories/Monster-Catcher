c742b0c1d08282c82b20b285ee336b74d523fa3f
Nortories <98294242+Nortories@users.noreply.github.com>
2023-05-17 11:51:53 -0600

    QOL

--- Monster_catcher_/Assets/Scripts/monster_encounter.cs
+++ Monster_catcher_/Assets/Scripts/monster_encounter.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
-
 public class monster_encounter : MonoBehaviour
 {
     [SerializeField] private GameObject playerTag;
-    [SerializeField][Range(1, 100)] private int encounterPercent;
+    [SerializeField][Range(1, 1000)] private int encounterProbablity;
     [SerializeField] TilemapCollider2D mosterCollider;
     [SerializeField] string sceneName;
     private Collider2D other;
     private static int min = 1;
     private static int max = 10000;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     private void OnTriggerStay2D(Collider2D other)
     {
@@ -35,10 +28,10 @@ public class monster_encounter : MonoBehaviour
         int randomNum = Random.Range(min, max);
         Debug.Log(randomNum);
 
-        if (randomNum < encounterPercent)
+        if (randomNum < encounterProbablity)
         {
-                SceneManager.LoadScene(sceneName);
-                //sceneManager changing to new scene not working
+            SceneManager.LoadScene(sceneName);
+            //sceneManager changing to new scene not working
         }
     }
 }
