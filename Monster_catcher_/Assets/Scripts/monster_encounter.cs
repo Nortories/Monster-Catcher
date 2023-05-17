042d6d54dd3514b8a877a08316d1b74d3804800b
Nortories <98294242+Nortories@users.noreply.github.com>
2023-05-17 09:35:41 -0600

    update

--- Monster_catcher_/Assets/Scripts/monster_encounter.cs
+++ Monster_catcher_/Assets/Scripts/monster_encounter.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading;
+
 
 public class monster_encounter : MonoBehaviour
 {
     [SerializeField] private Collider2D player;
+    [SerializeField] private int encounterPercent = 50;
+    [SerializeField] private float delay = 5;
     private BoxCollider2D monsterCollider;
-    
+    [SerializeField] private GameObject playerTag;
+
+    private Collider2D other;
+    bool running = false;
+    float timer;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +25,34 @@ public class monster_encounter : MonoBehaviour
     }
 
 
-    private void OnTriggerStay2D(Collider2D other){
-        if (other.gameObject.tag == "Player")
+    private void OnTriggerStay2D(Collider2D other)
+    {
+
+        running = true;
+        // if (running)
+        // {
+        //     Invoke("EncounterProb", delay);
+
+        //     running = false;
+        // }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        running = false;
+    }
+
+    private void EncounterProb()
+    {
+        int min = 0;
+        int max = 100;
+        if (Random.Range(min, max) < encounterPercent)
         {
-            SceneManager.LoadScene("TestScene");
-            //sceneManager changing to new scene not working
+            if (playerTag.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene("TestScene");
+                //sceneManager changing to new scene not working
+            }
         }
     }
 
@@ -27,5 +60,14 @@ public class monster_encounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    }
+
+    //     while (running)
+    //     {
+    //         timer += Time.deltaTime;
+    //         if (timer > delay)
+    //         {
+    //             EncounterProb();
+    //         }
+    //     }
+    // }
 }
